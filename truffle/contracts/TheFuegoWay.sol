3395eaf61f190db89e59201ea4bae7b3bc4b0d62
genialtechie <thehaleembello@gmail.com>
2022-10-11 17:09:18 -0400

    completed contract and tests

--- truffle/contracts/TheFuegoWay.sol
+++ truffle/contracts/TheFuegoWay.sol
@@ -2,19 +2,27 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TheFuegoWay is ERC721, Pausable, Ownable {
+contract TheFuegoWay is ERC721, ERC721Enumerable, Pausable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("The Fuego Way", "TFW") {}
+    constructor() ERC721("The Fuego Way", "TFW") {
+        _tokenIdCounter.increment();
+    }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmdkKnWMHcwMaifQrWsq8vr4D7hbWd54eHZKazqBW1YpuY/";
+        return "ipfs://QmcFkJih5rZVCocTc7L5uTBbN1SSuMbvZ5cL94m7urCoT1/";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
+        string memory uri = super.tokenURI(tokenId);
+        return string(abi.encodePacked(uri, ".json"));
     }
 
     function pause() public onlyOwner {
@@ -25,7 +33,7 @@ contract TheFuegoWay is ERC721, Pausable, Ownable {
         _unpause();
     }
 
-    function safeMint(address to) public onlyOwner {
+    function mint(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -34,8 +42,19 @@ contract TheFuegoWay is ERC721, Pausable, Ownable {
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         whenNotPaused
-        override
+        override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- truffle/test/thefuegoway.js
+++ truffle/test/thefuegoway.js
@@ -1,7 +1,35 @@
-// const NftContract = artifacts.require("TheFuegoWay");
+const NftContract = artifacts.require('TheFuegoWay');
 
-// contract('TheFuegoWay', () => {
-//   it('should read contract info', async() => {
-//     const instance = await NftContract.deployed();
-//   });
-// });
+let instance;
+
+beforeEach(async function () {
+  //create contract instance
+  instance = await NftContract.deployed();
+});
+
+contract('TheFuegoWay', (accounts) => {
+  it('should read contract info', async () => {
+    const name = await instance.name();
+    const symbol = await instance.symbol();
+
+    assert.equal(name, 'The Fuego Way');
+    assert.equal(symbol, 'TFW');
+  });
+
+  it('should mint a new token', async () => {
+    const result = await instance.mint(accounts[0]);
+    const totalSupply = await instance.totalSupply();
+    const tokenuri = await instance.tokenURI(1);
+
+    assert.equal(totalSupply, 1);
+    const event = result.logs[0].args;
+    assert.equal(event.tokenId.toNumber(), 1, 'id is correct');
+    assert.equal(
+      event.from,
+      '0x0000000000000000000000000000000000000000',
+      'from is correct'
+    );
+    assert.equal(event.to, accounts[0], 'to is correct');
+    console.log(`tokenuri: ${tokenuri}`);
+  });
+});
